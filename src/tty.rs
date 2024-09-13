14cfd0a8cbe79966f7b541ac5d833ef8cb44e401
catnapper321 <65686129+catnapper321@users.noreply.github.com>
2024-09-13 14:05:34 -0700

    Refactor tty code to builder pattern style

--- src/tty.rs
+++ src/tty.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 use std::mem::MaybeUninit;
 use std::{
-    os::fd::{AsFd, AsRawFd},
+    os::fd::{AsFd, AsRawFd, RawFd},
     io::{self, Read, Write, stdout, stdin},
     mem,
     ffi::CStr,
@@ -10,6 +10,8 @@ use libc::{
     c_int,
     termios,
 };
+mod password;
+pub use password::*;
 
 // input flags (iflag)
 pub use libc::{
@@ -53,144 +55,108 @@ impl SetAction {
     }
 }
 
-const STDOUT_DEFAULT_TERMIOS: std::cell::OnceCell<termios> = std::cell::OnceCell::new();
-
-const PASSWORD_BUFFER_LEN: usize = 512;
-/// Type that owns a heap allocated buffer that will not reallocate to
-/// accomodate input greater than PASSWORD_BUFFER_LEN bytes. When dropped,
-/// it overwrites the buffer contents with nul bytes. Users must ensure
-/// that the buffer contains at least one nul byte before calling
-/// [`as_cstr()`] or [`as_str()`].
-pub struct Password {
-    buf: Box<[u8; PASSWORD_BUFFER_LEN]>,
+#[derive(Debug)]
+pub struct Term {
+    fd: RawFd, 
+    orig_t: termios,
+    t: termios,
 }
-impl Password {
-    pub fn new() -> Self {
-        Self { buf: Box::new([0; PASSWORD_BUFFER_LEN]) }
+impl Term {
+    pub fn new(fd: impl AsRawFd) -> io::Result<Self> {
+        let fd = fd.as_raw_fd();
+        let orig_t = get_termios(fd)?;
+        Ok(Self {
+            fd,
+            orig_t,
+            t: orig_t.clone(),
+        })
     }
-    pub fn is_nul_terminated(&self) -> bool {
-        self.buf[PASSWORD_BUFFER_LEN - 1] == 0
+    /// raw mode: unset ECHO and ICANON, disable output flow control, disable
+    /// ctrl-v, disable carriage return translation (ctrl-m), disable ctrl-c
+    /// signalling, and some other stuff. Note that this function disables
+    /// output processing (auto carriage return insert).
+    pub fn raw_mode(&mut self) -> &mut Self {
+        self.t.c_iflag &= ! (IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+        self.t.c_lflag &= ! (ECHO | ICANON | IEXTEN | ISIG);
+        self.t.c_oflag &= ! (OPOST);
+        self.t.c_cflag &= ! libc::CS8;
+        self
     }
-    /// Panics if the buffer does not contain a nul byte
-    pub fn as_cstr(&self) -> &CStr {
-        CStr::from_bytes_until_nul(self.buf.as_slice()).expect("Password buffer requires terminating nul byte")
+    /// cooked mode: set ECHO, ECHONL, ICANON
+    /// note that this does the bare minimum to get utf input awareness and
+    /// character display; it does not restore flow control, unset the input
+    /// timeout, etc.
+    pub fn cooked_mode(&mut self) -> &mut Self {
+        self.t.c_lflag |= (ECHO | ECHONL | ICANON);
+        self
     }
-    pub fn as_str(&self) -> Result<&str, std::str::Utf8Error> {
-        self.as_cstr().to_str()
+    /// Turns off all output processing, like translating newline into
+    /// carriage return + newline
+    pub fn disable_output_processing(&mut self) -> &mut Self {
+        self.t.c_oflag &= ! OPOST;
+        self
     }
-    /// Convenience method for reading a newline terminated input from the
-    /// given Reader. Panics if the buffer would overflow. Removes the
-    /// trailing newline from the input.
-    pub fn read_line(&mut self, mut fd: impl Read) -> io::Result<()> {
-        let mut index = 0;
-        loop {
-            let buf = &mut self.buf[index..];
-            let n = fd.read(buf)?;
-            if n == 0 { break; }
-            index += n;
-            if index >= PASSWORD_BUFFER_LEN { panic!("Password buffer length exceeded"); }
-            if self.buf[index - 1] == b'\n' {
-                // replace the trailing newline with a nul byte
-                self.buf[index - 1] = 0;
-                break;
-            }
-        }
+    /// Enables output processing, so that newlines automatically have carriage
+    /// returns inserted before them
+    pub fn enable_output_processing(&mut self) -> &mut Self {
+        self.t.c_oflag |= OPOST;
+        self
+    }
+    /// password mode: unset ECHO, set ECHONL
+    pub fn password_mode(&mut self) -> &mut Self {
+        self.t.c_lflag &= ! ECHO;
+        self.t.c_lflag |= (ECHONL | ICANON);
+        self
+    }
+    /// disable output flow control (ctrl-s and ctrl-q)
+    pub fn disable_flow_control(&mut self) -> &mut Self {
+        self.t.c_iflag &= ! IXON;
+        self
+    }
+    /// enable output flow control (ctrl-s and ctrl-q)
+    pub fn enable_flow_control(&mut self) -> &mut Self {
+        self.t.c_iflag |= IXON;
+        self
+    }
+    /// Set input timeout, granularity is tenths of a second. Values over
+    /// 25.5s are set to 25.5s, values under 0.1s are set to 0.1s
+    pub fn input_timeout(&mut self, vtime: std::time::Duration) -> &mut Self {
+        self.t.c_cc[libc::VMIN] = 0; // return immediately after one byte read
+        let mut tenths = vtime.as_millis() / 100;
+        let tenths = match tenths {
+            0 => 1,
+            1..=255 => tenths as u8,
+            _ => 255,
+        };
+        self.t.c_cc[libc::VTIME] = tenths;
+        self
+    }
+    pub fn disable_input_timeout(&mut self) -> &mut Self {
+        self.t.c_cc[libc::VMIN] = 1;
+        self.t.c_cc[libc::VTIME] = 0;
+        self
+    }
+    /// Convenience function that sets the terminal to password
+    /// mode, prompts for a password, and resets the terminal. Uses
+    /// [`default_termios()`] internally, and so requires [`init()`] to be
+    /// called first.
+    pub fn prompt_for_password(&mut self, prompt: impl std::fmt::Display) -> io::Result<Password> {
+        self.password_mode().set(SetAction::TCSAFLUSH)?;
+        let mut pw = Password::new();
+        print!("{}: ", prompt);
+        stdout().flush()?;
+        pw.read_line(stdin())?;
+        self.reset(SetAction::TCSAFLUSH)?;
+        Ok(pw)
+    }
+    pub fn set(&self, action: SetAction) -> io::Result<()> {
+        set_termios(self.fd.clone(), action, &self.t)?;
         Ok(())
     }
-}
-impl Drop for Password {
-    fn drop(&mut self) {
-        self.buf.fill(0);
-        std::sync::atomic::compiler_fence(std::sync::atomic::Ordering::SeqCst);
-        std::sync::atomic::fence(std::sync::atomic::Ordering::SeqCst);
+    pub fn reset(&mut self, action: SetAction) -> io::Result<()> {
+        self.t = self.orig_t.clone();
+        self.set(action)
     }
-}
-impl std::ops::Deref for Password {
-    type Target = [u8; PASSWORD_BUFFER_LEN];
-
-    fn deref(&self) -> &Self::Target {
-        self.buf.as_ref()
-    }
-}
-impl std::ops::DerefMut for Password {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.buf.as_mut()
-    }
-}
-
-
-/// raw mode: unset ECHO and ICANON, disable output flow control, disable
-/// ctrl-v, disable carriage return translation (ctrl-m), disable ctrl-c
-/// signalling, and some other stuff. Note that this function disables
-/// output processing (auto carriage return insert).
-pub fn termios_set_raw_mode(t: &mut termios) {
-    t.c_iflag &= ! (IXON | ICRNL | BRKINT | INPCK | ISTRIP);
-    t.c_lflag &= ! (ECHO | ICANON | IEXTEN | ISIG);
-    t.c_oflag &= ! (OPOST);
-    t.c_cflag &= ! libc::CS8;
-}
-
-/// cooked mode: set ECHO, ECHONL, ICANON
-/// note that this does the bare minimum to get utf input awareness and
-/// character display; it does not restore flow control, unset the input
-/// timeout, etc.
-pub fn termios_set_cooked_mode(t: &mut termios) {
-    t.c_lflag |= (ECHO | ECHONL | ICANON);
-}
-
-/// Turns off all output processing, like translating newline into
-/// carriage return + newline
-pub fn termios_disable_output_processing(t: &mut termios) {
-    t.c_oflag &= ! OPOST
-}
-
-/// Enables output processing, so that newlines automatically have carriage
-/// returns inserted before them
-pub fn termios_enable_output_processing(t: &mut termios) {
-    t.c_oflag |= OPOST
-}
-
-/// password mode: unset ECHO, set ECHONL
-pub fn termios_set_password_mode(t: &mut termios) {
-    t.c_lflag &= ! ECHO;
-    t.c_lflag |= (ECHONL | ICANON);
-}
-
-/// disable output flow control (ctrl-s and ctrl-q)
-pub fn termios_disable_flow_control(t: &mut termios) {
-    t.c_iflag &= ! IXON;
-}
-/// enable output flow control (ctrl-s and ctrl-q)
-pub fn termios_enable_flow_control(t: &mut termios) {
-    t.c_iflag |= IXON;
-}
-/// Set input timeout, granularity is tenths of a second. Values over
-/// 25.5s are set to 25.5s, values under 0.1s are set to 0.1s
-pub fn termios_set_input_timeout(t: &mut termios, vtime: std::time::Duration) {
-    t.c_cc[libc::VMIN] = 0; // return immediately after one byte read
-    let mut tenths = vtime.as_millis() / 100;
-    let tenths = match tenths {
-        0 => 1,
-        1..=255 => tenths as u8,
-        _ => 255,
-    };
-    t.c_cc[libc::VTIME] = tenths;
-}
-
-/// Convenience function that sets the terminal on stdout to password mode,
-/// prompts for a password, and resets the terminal. Uses [`default_termios()`]
-/// internally, and so requires [`init()`] to be called first.
-pub fn get_password(prompt: impl std::fmt::Display) -> io::Result<Password> {
-    let mut t = get_termios(stdout())?;
-    let orig_t = get_termios(stdout())?;
-    termios_set_password_mode(&mut t);
-    set_termios(stdout(), SetAction::TCSAFLUSH, &t)?;
-    let mut pw = Password::new();
-    print!("{}: ", prompt);
-    stdout().flush()?;
-    pw.read_line(stdin())?;
-    set_termios(std::io::stdout(), SetAction::TCSAFLUSH, &orig_t)?;
-    Ok(pw)
 }
 
 /// Safe wrapper around `libc::tcgetattr`. Returns a `libc::termios`.
---
+++ src/tty/password.rs
@@ -0,0 +1,69 @@
+use std::{
+    ffi::CStr,
+    io::{self, Read},
+};
+
+const PASSWORD_BUFFER_LEN: usize = 512;
+/// Type that owns a heap allocated buffer that will not reallocate to
+/// accomodate input greater than PASSWORD_BUFFER_LEN bytes. When dropped,
+/// it overwrites the buffer contents with nul bytes. Users must ensure
+/// that the buffer contains at least one nul byte before calling
+/// [`as_cstr()`] or [`as_str()`].
+pub struct Password {
+    buf: Box<[u8; PASSWORD_BUFFER_LEN]>,
+}
+impl Password {
+    pub fn new() -> Self {
+        Self { buf: Box::new([0; PASSWORD_BUFFER_LEN]) }
+    }
+    pub fn is_nul_terminated(&self) -> bool {
+        self.buf[PASSWORD_BUFFER_LEN - 1] == 0
+    }
+    /// Panics if the buffer does not contain a nul byte
+    pub fn as_cstr(&self) -> &CStr {
+        CStr::from_bytes_until_nul(self.buf.as_slice()).expect("Password buffer requires terminating nul byte")
+    }
+    pub fn as_str(&self) -> Result<&str, std::str::Utf8Error> {
+        self.as_cstr().to_str()
+    }
+    /// Convenience method for reading a newline terminated input from the
+    /// given Reader. Removes the trailing newline from the input. If the
+    /// input is larger than `PASSWORD_BUFFER_LEN`, returns
+    /// std::io::ErrorKind::InvalidData.
+    pub fn read_line(&mut self, mut fd: impl Read) -> io::Result<()> {
+        let mut index = 0;
+        loop {
+            let buf = &mut self.buf[index..];
+            let n = fd.read(buf)?;
+            if n == 0 { break; }
+            index += n;
+            if index >= PASSWORD_BUFFER_LEN { return Err(io::ErrorKind::InvalidData.into()); }
+            if self.buf[index - 1] == b'\n' {
+                // replace the trailing newline with a nul byte
+                self.buf[index - 1] = 0;
+                break;
+            }
+        }
+        Ok(())
+    }
+}
+impl Drop for Password {
+    fn drop(&mut self) {
+        self.buf.fill(0);
+        std::sync::atomic::compiler_fence(std::sync::atomic::Ordering::SeqCst);
+        std::sync::atomic::fence(std::sync::atomic::Ordering::SeqCst);
+    }
+}
+impl std::ops::Deref for Password {
+    type Target = [u8; PASSWORD_BUFFER_LEN];
+
+    fn deref(&self) -> &Self::Target {
+        self.buf.as_ref()
+    }
+}
+impl std::ops::DerefMut for Password {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.buf.as_mut()
+    }
+}
+
